ac1f87f11cf11432effff9faf0a582d851675158
Lehlohonolo Letaoana <lehlohonolo.letaoana@derivco.co.za>
2023-04-19 19:10:52 +0200

    first commit

---
+++ ListManipulator.App/ListManipulator.cs
@@ -0,0 +1,56 @@
+﻿namespace ListManipulator.App
+{
+    public class ListManipulator
+    {
+        private List<int> NegativeNumbers { get; init; }
+        private List<int> PositiveNumbers { get; init; }
+
+        public ListManipulator()
+        {
+            NegativeNumbers = new List<int>();
+            PositiveNumbers = new List<int>();
+        }
+
+        public void PrintTheNumbers(IReadOnlyList<string> input)
+        {
+            if (input.Any())
+            {
+                DivideTheInput(input);
+                var finalList = GetFinalList();
+                var output = string.Join(',', finalList);
+                Console.Write(output);
+                Console.Read();
+            }
+            else
+                Console.WriteLine("No input provided, please try again.");
+        }
+
+        private List<string> GetFinalList()
+        {
+            var finalList = new List<string>();
+            if (NegativeNumbers.Any())
+            {
+                finalList.Add("|");
+                for (var i = 0; i < PositiveNumbers.Count; i++)
+                    finalList.Add(i.ToString());
+            }
+            else
+                for (var j = 0; j < PositiveNumbers.Count; j++)
+                    finalList.Add(j.ToString());
+
+            return finalList;
+        }
+
+        private void DivideTheInput(IEnumerable<string> input)
+        {
+            foreach (var t in input)
+            {
+                var x = int.Parse(t.Trim());
+                if (x < 0)
+                    NegativeNumbers.Add(int.Parse(t));
+                else
+                    PositiveNumbers.Add(int.Parse(t));
+            }
+        }
+    }
+}
---
+++ ListManipulator.UI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace ListManipulator.UI
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var continueCapturing = true;
+            var sb = new StringBuilder();
+            while (continueCapturing)
+            {
+                PromptUserForInput();
+                var number = ProcessUserInput();
+                if (string.IsNullOrEmpty(number)) continue;
+                if (ExitApplication(number) || number == "p")
+                {
+                    continueCapturing = false;
+                    continue;
+                }
+                sb.Append(sb.Length > 0 ? $", {number}" : number);
+            }
+
+            var app = new App.ListManipulator();
+            app.PrintTheNumbers(sb.ToString().Split(",").ToList());
+        }
+
+        private static bool ExitApplication(string number)
+        {
+            return number == "x";
+        }
+
+        private static string ProcessUserInput()
+        {
+            var userInput = CaptureUserInput();
+            if (userInput == null) return string.Empty;
+            return int.TryParse(userInput, out var number) ? number.ToString() : userInput;
+        }
+
+        private static void PromptUserForInput()
+        {
+            Console.WriteLine("Type in a number to continue | Type in x to exit the application | Type in p to print out the numbers...");
+        }
+
+        private static string CaptureUserInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
